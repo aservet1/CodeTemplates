fd11d52117266f38df0dd1fbefd81281fa482b1e
aservet1 <54599694+aservet1@users.noreply.github.com>
2021-01-02 23:38:20 -0500

    add note to only run it from the correct directory

--- scripts/setup-instructions.sh
+++ scripts/setup-instructions.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# make sure you run this in the directory that you would like to officially export the name to $CODETEMPL_ROOT.
+# it will be inaccurate if you run it from any other directory
+
 echo 'copy the lines below into $HOME/.profile to set the CODETEMPL_ROOT environment variable and add it to PATH '
 echo
 echo '# setup for codetempl'
